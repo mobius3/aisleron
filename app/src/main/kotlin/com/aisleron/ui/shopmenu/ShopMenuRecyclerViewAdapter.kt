e31d274eff11ef3d81bfc9e862e75a99b310a239
thebatdan <github@roux.one>
2026-03-27 19:58:42 +1300

    Fix incorrect pinned shop being displayed after reordering shops.
    Signed-off-by: thebatdan <github@roux.one>

--- app/src/main/kotlin/com/aisleron/ui/shopmenu/ShopMenuRecyclerViewAdapter.kt
+++ app/src/main/kotlin/com/aisleron/ui/shopmenu/ShopMenuRecyclerViewAdapter.kt
@@ -23,38 +23,36 @@ import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.aisleron.databinding.FragmentShopMenuItemBinding
-import com.aisleron.domain.location.Location
 import com.aisleron.ui.shoplist.ShopListItemDiffCallback
 import com.aisleron.ui.shoplist.ShopListItemViewModel
 
-/**
- * [RecyclerView.Adapter] that can display a [Location].
- *
- */
 class ShopMenuRecyclerViewAdapter(
     private val listener: ShopMenuItemListener
 ) : ListAdapter<ShopListItemViewModel, ShopMenuRecyclerViewAdapter.ViewHolder>(
     ShopListItemDiffCallback()
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
+        val newViewHolder = ViewHolder(
             FragmentShopMenuItemBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
             )
         )
+
+        newViewHolder.itemView.setOnClickListener {
+            listener.onClick(getItem(newViewHolder.absoluteAdapterPosition))
+        }
+
+        return newViewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         holder.contentView.text = item.name
-        holder.itemView.setOnClickListener {
-            listener.onClick(getItem(position))
-        }
     }
 
-    inner class ViewHolder(binding: FragmentShopMenuItemBinding) :
+    class ViewHolder(binding: FragmentShopMenuItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         val contentView: TextView = binding.txtShopName
     }
